8746d9d0d3ea448c7c7943e7616cf4bee4d6888f
Long <luonggialong195@gmail.com>
2019-11-07 14:31:27 +0700

    Spinner when fetching projects data

--- src/components/Project/ProjectIndex.jsx
+++ src/components/Project/ProjectIndex.jsx
@@ -5,20 +5,40 @@ import { fetchProjects } from '../../actions/projectsAction'
 import { connect } from 'react-redux'
 
 class CProjectIndex extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   componentDidMount() {
-    if(!this.props.projects.length)
+    if(!this.props.projects.length) {
       this.props.fetchProjects();
     }
+  }
 
   render() {
     const projects = this.props.projects || []
 
+    let tableData
+    if(projects.length) {
+      tableData = <MyTable data={projects}/>
+    } else {
+      tableData = <div>
+          <div className="spinner-grow text-muted"></div>
+          <div className="spinner-grow text-primary"></div>
+          <div className="spinner-grow text-success"></div>
+          <div className="spinner-grow text-info"></div>
+          <div className="spinner-grow text-warning"></div>
+          <div className="spinner-grow text-danger"></div>
+          <div className="spinner-grow text-secondary"></div>
+          <div className="spinner-grow text-dark"></div>
+          <div className="spinner-grow text-light"></div>
+      </div>;
+    }
     return (
       <Fragment>
         <h2 className="my-5">PROJECTS MANAGEMENT</h2>
         <Link className="btn btn-primary my-3" to="/projects/new"><b>+</b> New Project</Link>
-        <MyTable data={projects}/>
+        { tableData  }
       </Fragment>
     )
   }
